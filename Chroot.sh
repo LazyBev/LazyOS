80185db40d7aee334251ade97ee266d4a0ffe23b
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-30 07:10:21 +0000

    Update Chroot.sh

--- Chroot.sh
+++ Chroot.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+set -eau
+
+#Variables
+export LFS=/mnt/lfs
+export LFS_TGT=$(uname -m)-lfs-linux-gnu
+export LC_ALL=C 
+export PATH=/usr/bin:/bin
+
+chown --from lfs -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+  x86_64) chown --from lfs -R root:root $LFS/lib64 ;;
+esac
