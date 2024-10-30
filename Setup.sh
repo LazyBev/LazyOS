69931951d044200a91a2f06f94c9b44ae0dacb9f
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-30 03:06:17 +0000

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -20,8 +20,9 @@ su root -c "[ ! -e /etc/bash.bashrc ] || mv -v /etc/bash.bashrc /etc/bash.bashrc
 
 source ~/.bash_profile
 
-cd $LFS/sources && tar -xf binutils*
-cd binutils*/ && mkdir -v build
+# Binutils
+cd $LFS/sources && tar -xf binutils*.tar.xz && cd binutils*/ 
+mkdir -v build && cd build
 ../configure --prefix=$LFS/tools \
              --with-sysroot=$LFS \
              --target=$LFS_TGT   \
@@ -30,9 +31,11 @@ cd binutils*/ && mkdir -v build
              --disable-werror    \
              --enable-new-dtags  \
              --enable-default-hash-style=gnu
-make && make install
+make -j$(nproc) && make install
+rm -rf $LFS/sources/binutils*.tar.xz
 
-cd $LFS/sources && tar -xf gcc* && cd gcc*/
+# Gcc
+cd $LFS/sources && tar -xf gcc*.tar.xz && cd gcc*/
 tar -xf ../mpfr-4.2.1.tar.xz && mv -v mpfr-4.2.1 mpfr
 tar -xf ../gmp-6.3.0.tar.xz && mv -v gmp-6.3.0 gmp
 tar -xf ../mpc-1.3.1.tar.gz && mv -v mpc-1.3.1 mpc
@@ -65,6 +68,44 @@ mkdir -v build && cd build
     --disable-libvtv          \
     --disable-libstdcxx       \
     --enable-languages=c,c++
-make && make install
+make -j$(nproc) && make install
 cd .. && cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
   `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+rm -rf $LFS/sources/gcc*.tar.xz
+
+# Linux kernel
+cd $LFS/sources && tar -xf linux*.tar.xz && cd linux*/
+make mrproper && make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
+rm -rf $LFS/sources/linux*.tar.xz
+
+# Glibc
+cd $LFS/sources && tar -xf glibc*.tar.xz && cd glibc*
+case $(uname -m) in
+    i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+    ;;
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+    ;;
+esac
+patch -Np1 -i ../glibc-2.40-fhs-1.patch
+mkdir -v build && cd build
+echo "rootsbindir=/usr/sbin" > configparms
+../configure                             \
+      --prefix=/usr                      \
+      --host=$LFS_TGT                    \
+      --build=$(../scripts/config.guess) \
+      --enable-kernel=4.19               \
+      --with-headers=$LFS/usr/include    \
+      --disable-nscd                     \
+      libc_cv_slibdir=/usr/lib
+make -j$(nproc) && make DESTDIR=$LFS install
+sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+echo 'int main(){}' | $LFS_TGT-gcc -xc -
+readelf -l a.out | grep ld-linux
+rm -v a.out
+rm -rf $LFS/sources/glibc*.tar.xz
+
+# Libstdc++
+cd $LFS/sources && tar -xf lib*.tar.xz && cd linux*/
