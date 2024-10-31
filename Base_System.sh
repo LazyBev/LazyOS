483f2cae4c2e2bd99821b21f6aabe7bb6a177771
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-31 15:53:02 +0000

    Update Base_System.sh

--- Base_System.sh
+++ Base_System.sh
@@ -1310,9 +1310,64 @@ read -p "Type in a hostname for your system: " hsnm
 
 echo "$hsnm" > /etc/hostname
 
+cat > /etc/inittab << "INIT"
+id:3:initdefault:
 
+si::sysinit:/etc/rc.d/init.d/rc S
 
+l0:0:wait:/etc/rc.d/init.d/rc 0
+l1:S1:wait:/etc/rc.d/init.d/rc 1
+l2:2:wait:/etc/rc.d/init.d/rc 2
+l3:3:wait:/etc/rc.d/init.d/rc 3
+l4:4:wait:/etc/rc.d/init.d/rc 4
+l5:5:wait:/etc/rc.d/init.d/rc 5
+l6:6:wait:/etc/rc.d/init.d/rc 6
 
+ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now
+
+su:S06:once:/sbin/sulogin
+s1:1:respawn:/sbin/sulogin
+
+1:2345:respawn:/sbin/agetty --noclear tty1 9600
+2:2345:respawn:/sbin/agetty tty2 9600
+3:2345:respawn:/sbin/agetty tty3 9600
+4:2345:respawn:/sbin/agetty tty4 9600
+5:2345:respawn:/sbin/agetty tty5 9600
+6:2345:respawn:/sbin/agetty tty6 9600
+INIT
+
+cat > /etc/sysconfig/clock << "SYSCLOCK"
+UTC=1
+# Set this to any options you might need to give to hwclock,
+# such as machine hardware clock type for Alphas.
+CLOCKPARAMS=
+SYSCLOCK
+
+cat > /etc/sysconfig/console << "SYSCONSOLE"
+KEYMAP="uk"
+FONT="lat2a-16 -m 8859-1"
+LOGLEVEL="3"
+SYSCONSOLE
+
+echo -e "\nFind your locale and remember it."; sleep 3 && locale -a | less && \
+read -p "What locale do you want: " loc && LC_ALL=$loc 
+export locchar=$(locale charmap)
+
+if [[ "$loc" == *.* ]]; then
+	loc="${loc%%.*}"
+fi
+
+cat > /etc/profile << "PROF"
+for i in $(locale); do
+  unset ${i%=*}
+done
+
+if [[ "$TERM" = linux ]]; then
+  export LANG=C.UTF-8
+else
+  export LANG=$loc.$locchar
+fi
+PROF
 
 
 EOF
