8ec722eedea7c20f644ed7371d95c8319e9c5e3c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-31 22:38:22 +0000

    Update Base_System.sh

--- Base_System.sh
+++ Base_System.sh
@@ -1617,8 +1617,21 @@ cd /sources && tar -xvJf git*.tar.xz && cd git*/
 make
 cd /sources
 
-
-
+# Cpio
+tar -xvjf cpio*.tar.bz2 && cd cpio*/
+./configure --prefix=/usr \
+            --enable-mt   \
+            --with-rmt=/usr/libexec/rmt &&
+make &&
+makeinfo --html            -o doc/html      doc/cpio.texi &&
+makeinfo --html --no-split -o doc/cpio.html doc/cpio.texi &&
+makeinfo --plaintext       -o doc/cpio.txt  doc/cpio.texi
+make install &&
+install -v -m755 -d /usr/share/doc/cpio-2.15/html &&
+install -v -m644    doc/html/* \
+                    /usr/share/doc/cpio-2.15/html &&
+install -v -m644    doc/cpio.{html,txt} \
+                    /usr/share/doc/cpio-2.15
 
 
 EOF
