e6d47c2f9a2c667ef4f3c14591a190629cf214f2
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-31 12:52:55 +0000

    Update Base_System.sh

--- Base_System.sh
+++ Base_System.sh
@@ -1003,17 +1003,24 @@ rm -fv man/man8/arpd.8
 make -j$(nproc) NETNS_RUN_DIR=/run/netns && make SBINDIR=/usr/sbin install
 mkdir -pv /usr/share/doc/iproute2-6.10.0
 cp -v COPYING README* /usr/share/doc/iproute2-6.10.0
+cd /sources
 
+# Kbd
+tar -xvJf kbd*.tar.xz && cd kbd*/
+patch -Np1 -i ../kbd-2.6.4-backspace-1.patch
+sed -i '/RESIZECONS_PROGS=/s/yes/no/' configure
+sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in
+./configure --prefix=/usr --disable-vlock
+make -j$(nproc) && make check
+make install
+cp -R -v docs/doc -T /usr/share/doc/kbd-2.6.4
+cd /sources
 
-
-
-
-
-
-
-
-
-
+# Libpipeline
+tar -xvzf libpipeline*.tar.gz && cd libpipeline*/
+./configure --prefix=/usr
+make -j$(nproc) && make check
+make install
 
 
 
